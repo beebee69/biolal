1175093d3f2014e1d05a285dcb3250c214f8ab04
beebee69 <abiolaakinola7@gmail.com>
2014-07-21 16:16:36 +0100

    Create GruntFile.js

---
+++ GruntFile.js
@@ -0,0 +1,14 @@
+ connect: {
+        	server: {
+        		options: {
+        			port: 2013,
+        			base: './dist'
+        		}
+        	}
+@@ -24,7 +24,7 @@ module.exports = function (grunt) {
+
+        open: {
+            dev: {
+            	path: 'http://localhost:2013/index.html'
+            }
+        },
